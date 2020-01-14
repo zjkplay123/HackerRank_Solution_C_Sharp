be4c8466eaff533b604fd32e3ba056426453cb49
Jinkai Zhang <56803578+zjkplay123@users.noreply.github.com>
2020-01-14 20:57:19 +0000

    Mini-Max Sum

---
+++ Mini-Max Sum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HackerRank
+{
+    class Solution
+    {
+
+        // Complete the staircase function below.
+        static void staircase(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                int k = n - i;
+                for (int j = 1; j <= k; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.Write("\n");
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            staircase(n);
+        }
+    }
+}
