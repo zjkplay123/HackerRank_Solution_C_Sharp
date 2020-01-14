4ac7570281871f10a600ca1086adbb0c174b52bd
Jinkai Zhang <56803578+zjkplay123@users.noreply.github.com>
2020-01-14 16:01:19 +0000

    A_Very_Big_Sum

---
+++ A Very Big Sum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HackerRank
+{
+    class Program
+    {
+        // Complete the aVeryBigSum function below.
+        static long aVeryBigSum(long[] ar)
+        {
+            long results = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                results = results + ar[i];
+            }
+            return results;
+        }
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int arCount = Convert.ToInt32(Console.ReadLine());
+
+            long[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt64(arTemp))
+            ;
+            long result = aVeryBigSum(ar);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
